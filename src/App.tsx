89d616ed0955052c1312e54afd556b43cdf14587
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-03-18 10:58:33 +0530

    Issue #215560 feat: reusable modal component added with sample usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ModalComponent from "./components/Modal.tsx";
+import { useState } from "react";
+import { Button, TextField, Radio, Box } from "@mui/material";
+import CustomButton from "./components/CustomButton.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [nestedModalOpen, setNestedModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const openNestedModal = () => {
+    setNestedModalOpen(true);
+  }
+
+  const closeNestedModal = () => {
+    setNestedModalOpen(false);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button onClick={handleOpenModal}>Open Modal</Button>
+      <ModalComponent open={modalOpen} onClose={handleCloseModal} heading={'Mark attendance'} SubHeading={'date'} btnText="apply">
+        <TextField label="Input Field" variant="outlined" />
+        <Box>
+          <Radio checked={true} />
+          <span>Option 1</span>
+          <Radio />
+          <span>Option 2</span>
+        </Box>
+        <Button onClick={openNestedModal}>Open Nested Modal</Button>
+        <Button onClick={handleCloseModal}>Close</Button>
+      </ModalComponent >
+
+      {nestedModalOpen && (
+        <ModalComponent open={nestedModalOpen} onClose={closeNestedModal} heading={'nested modal'} SubHeading={'subheading'} btnText="apply">
+          <TextField label="Input Field" variant="outlined" />
+        </ModalComponent>
+      )}
     </>
   )
 }
