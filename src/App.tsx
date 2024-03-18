cf376a9e5f661b8b38dd944f45ce660e4deb220a
Rushikesh-Sonawane99 <rushikesh.sonawane@tekditechnologies.com>
2024-03-18 14:26:03 +0530

    Issue #215560 feat: Created PageHeaderComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import ModalUsage from './pages/ModalUsage'
+import React from "react";
+import "./App.css";
+import PageHeaderComponent from "./components/PageHeaderComponent";
+import ModalUsage from "./pages/ModalUsage";
+// import StudentsStatsListView from './components/StudentsStatsListView'
+// import StudentStatsCard from './components/StudentStatsCard'
 
 function App() {
-
-
   return (
     <>
+      {/* <StudentsStatsListView studentName="Rushi" label1="Attendance" value1="78%" label2="Missed Casses" value2="2"/> */}
+      {/* <StudentStatsCard label1="Attendance" value1="78%" label2 = {true} value2={"02/01/2022"}/> */}
+      <PageHeaderComponent
+        label1="Attendance"
+        value1="78%"
+        label2={true}
+        value2={"02/01/2022"}
+      />
       <ModalUsage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
