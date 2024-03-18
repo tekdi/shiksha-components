1738a907d86abf35afd87a61406626f40b584dba
suvarnakale <suvarna.kale@tekditechnologies.com>
2024-03-18 11:08:26 +0530

    PR updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
-import ModalComponent from "./components/Modal.tsx";
-import { useState } from "react";
-import { Button, TextField, Radio, Box } from "@mui/material";
-import CustomButton from "./components/CustomButton.tsx";
+import React from 'react'
+import ModalUsage from './pages/ModalUsage'
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [nestedModalOpen, setNestedModalOpen] = useState(false);
 
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
-
-  const openNestedModal = () => {
-    setNestedModalOpen(true);
-  }
-
-  const closeNestedModal = () => {
-    setNestedModalOpen(false);
-  }
 
   return (
     <>
-      <Button onClick={handleOpenModal}>Open Modal</Button>
-      <ModalComponent open={modalOpen} onClose={handleCloseModal} heading={'Mark attendance'} SubHeading={'date'} btnText="apply">
-        <TextField label="Input Field" variant="outlined" />
-        <Box>
-          <Radio checked={true} />
-          <span>Option 1</span>
-          <Radio />
-          <span>Option 2</span>
-        </Box>
-        <Button onClick={openNestedModal}>Open Nested Modal</Button>
-        <Button onClick={handleCloseModal}>Close</Button>
-      </ModalComponent >
-
-      {nestedModalOpen && (
-        <ModalComponent open={nestedModalOpen} onClose={closeNestedModal} heading={'nested modal'} SubHeading={'subheading'} btnText="apply">
-          <TextField label="Input Field" variant="outlined" />
-        </ModalComponent>
-      )}
+      <ModalUsage />
     </>
   )
 }
