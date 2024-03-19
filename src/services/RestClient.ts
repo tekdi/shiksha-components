d99b97ad4161bafbd9ecd9bcf2b4592810d309fd
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2024-03-19 11:38:19 +0530

    update PR: remove await from return value

--- src/services/RestClient.ts
+++ src/services/RestClient.ts
@@ -2,21 +2,21 @@ import axiosInstance from './Interceptor';
 import  { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 export async function get<T>(url: string, headers: AxiosRequestConfig['headers'] = {}): Promise<AxiosResponse<T>> {
-  return await axiosInstance.get<T>(url, { headers });
+  return  axiosInstance.get<T>(url, { headers });
 }
 
 export async function post<T>(url: string, body: any, headers: AxiosRequestConfig['headers'] = {}): Promise<AxiosResponse<T>> {
-  return await axiosInstance.post<T>(url, body, { headers });
+  return  axiosInstance.post<T>(url, body, { headers });
 }
 
 export async function put<T>(url: string, body: any, headers: AxiosRequestConfig['headers'] = {}): Promise<AxiosResponse<T>> {
-  return await axiosInstance.put<T>(url, body, { headers });
+  return  axiosInstance.put<T>(url, body, { headers });
 }
 
 export async function patch<T>(url: string, body: any, headers: AxiosRequestConfig['headers'] = {}): Promise<AxiosResponse<T>> {
-  return await axiosInstance.patch<T>(url, body, { headers });
+  return  axiosInstance.patch<T>(url, body, { headers });
 }
 
 export async function deleteApi<T>(url: string, body: any, headers: AxiosRequestConfig['headers'] = {}): Promise<AxiosResponse<T>> {
-  return await axiosInstance.delete<T>(url, { data: body, headers });
+  return  axiosInstance.delete<T>(url, { data: body, headers });
 }
