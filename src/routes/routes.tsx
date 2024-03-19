21daa658104bb73899e46856f615ca315421a64f
rahul_g <rahul_g@tekditechnologies.com>
2024-03-19 18:39:25 +0530

    added routes used lazy loading

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,16 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { TextField } from "@mui/material";
 
 const Test = lazy(() => import("../components/Test"));
 
 function AllRoutes(): JSX.Element {
+  const { t } = useTranslation();
+
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<TextField>{t("COMMON.LOADING")}</TextField>}>
         <Routes>
           <Route path="/test" element={<Test />} />
         </Routes>
