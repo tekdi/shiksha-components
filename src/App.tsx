3aa0afafeb502f9f295a3f3c4a81021ea12f9c17
rahul_g <rahul_g@tekditechnologies.com>
2024-03-20 11:46:08 +0530

    resolved conflicts issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import AllRoutes from "./routes/routes";
-import "./App.css";
+import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+import AllRoutes from "./routes/routes.tsx";
 
 import customTheme from "./customStyles.tsx";
 
 function App() {
   return (
-    <>
-      <ThemeProvider theme={customTheme}>
-        <AllRoutes />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={customTheme}>
+      <AllRoutes />
+    </ThemeProvider>
   );
 }
 
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { FC } from "react";
 
-const Test = () => {
+// Define the props interface if the component takes any props
+interface TestProps {
+  // Define props here if needed
+}
+
+// Define the Test component with its props
+const Test: FC<TestProps> = () => {
   return <div>Test</div>;
 };
 
