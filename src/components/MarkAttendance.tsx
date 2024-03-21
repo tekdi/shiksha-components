ad685b115667982e44b6dd947482e22882808545
s-nawaz <nawaz.shaik@tekditechnologies.com>
2024-03-21 12:44:10 +0530

    Mark attendance customization going so that we can use it for both student and teacher

--- src/components/MarkAttendance.tsx
+++ src/components/MarkAttendance.tsx
@@ -1,142 +1,145 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
 //components
-import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogActions from "@mui/material/DialogActions";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 //icons
-import CheckCircleIcon from "@mui/icons-material/CheckCircle"; //present
-import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import CancelIcon from "@mui/icons-material/Cancel"; //absent
-import CloseIcon from "@mui/icons-material/Close";
-import RemoveCircleIcon from "@mui/icons-material/RemoveCircle"; //Half-Day
-import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-//colors
-import { grey, amber } from "@mui/material/colors";
-import { Box } from "@mui/material";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle'; //present
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CancelIcon from '@mui/icons-material/Cancel'; //absent
+import CloseIcon from '@mui/icons-material/Close';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle'; //Half-Day
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(2),
-  },
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(2),
+    },
 }));
 
 const ATTENDANCE_ENUM = {
-  PRESENT: "PRESENT",
-  ABSENT: "ABSENT",
-  HALFDAY: "HALF_DAY",
-};
+    PRESENT: "present",
+    ABSENT: "absent",
+    HALF_DAY: "halfday",
+    NOT_MARKED: "notmarked"
+}
 
-interface MarkAttendanceProps {}
+interface MarkAttendanceProps {
+    isOpen: boolean,
+    title: string,
+    date: string,
+    name?: string,
+    currentStatus: string,
+    handleClose: () => void,
+    handleSubmit: () => void,
+}
 
-const MarkAttendance: React.FC<MarkAttendanceProps> = ({}) => {
-  const [open, setOpen] = React.useState(false);
-  const [status, setStatus] = React.useState("");
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Mark Self Attendance
-      </Button>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-          Mark My Attendance
-        </DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={handleClose}
-          sx={{
-            position: "absolute",
-            right: 8,
-            top: 8,
-            color: grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent dividers>
-          <Box display="flex" flexDirection="row" justifyContent="space-around">
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              p={2}
-              onClick={() => setStatus(ATTENDANCE_ENUM.PRESENT)}
+const MarkAttendance: React.FC<MarkAttendanceProps> = ({
+    isOpen,
+    title,
+    date,
+    name,
+    currentStatus,
+    handleClose,
+    handleSubmit
+}) => {
+    const [status, setStatus] = React.useState(currentStatus)
+    const theme = useTheme<any>();
+    console.log({ name });
+    return (
+        <React.Fragment>
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={isOpen}
             >
-              {status === ATTENDANCE_ENUM.PRESENT ? (
-                <CheckCircleIcon />
-              ) : (
-                <CheckCircleOutlineIcon />
-              )}
-              <Typography marginTop={1}>Present</Typography>
-            </Box>
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              p={2}
-              onClick={() => setStatus(ATTENDANCE_ENUM.ABSENT)}
-            >
-              {status === ATTENDANCE_ENUM.ABSENT ? (
-                <CancelIcon />
-              ) : (
-                <CloseIcon />
-              )}
-              <Typography marginTop={1}>Absent</Typography>
-            </Box>
-            <Box
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              p={2}
-              onClick={() => setStatus(ATTENDANCE_ENUM.HALFDAY)}
-            >
-              {status === ATTENDANCE_ENUM.HALFDAY ? (
-                <RemoveCircleIcon />
-              ) : (
-                <RemoveCircleOutlineIcon />
-              )}
-              <Typography marginTop={1}>Half Day</Typography>
-            </Box>
-          </Box>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            variant="contained"
-            autoFocus
-            onClick={handleClose}
-            disabled={status === ""}
-            sx={{
-              width: "100%",
-              borderRadius: 8,
-              color: grey[900],
-              backgroundColor: amber[500],
-            }}
-          >
-            Save
-          </Button>
-        </DialogActions>
-      </BootstrapDialog>
-    </React.Fragment>
-  );
-};
+                <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                    <Typography variant="h2" sx={{ marginBottom: 0 }}>{title}</Typography>
+                    <Typography variant="h4" sx={{ marginBottom: 0, color: theme.palette.warning["A200"] }}>{date}</Typography>
+                </DialogTitle>
+                {/* <Typography variant="h2">Mark Attendance</Typography> */}
+                <IconButton
+                    aria-label="close"
+                    onClick={handleClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: theme.palette.warning["A200"],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+                <DialogContent dividers>
+                    <Box display="flex" flexDirection="row" justifyContent="space-around">
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            alignItems="center"
+                            p={2}
+                            onClick={() => setStatus(ATTENDANCE_ENUM.PRESENT)}
+                        >
+                            {status === ATTENDANCE_ENUM.PRESENT ? <CheckCircleIcon /> : <CheckCircleOutlineIcon />}
+                            <Typography marginTop={1}>Present</Typography>
+                        </Box>
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            alignItems="center"
+                            p={2}
+                            onClick={() => setStatus(ATTENDANCE_ENUM.ABSENT)}
+                        >
+                            {status === ATTENDANCE_ENUM.ABSENT ? <CancelIcon /> : <CloseIcon />}
+                            <Typography marginTop={1}>Absent</Typography>
+                        </Box>
+                        <Box
+                            display="flex"
+                            flexDirection="column"
+                            alignItems="center"
+                            p={2}
+                            onClick={() => setStatus(ATTENDANCE_ENUM.HALF_DAY)}
+                        >
+                            {status === ATTENDANCE_ENUM.HALF_DAY ? <RemoveCircleIcon /> : <RemoveCircleOutlineIcon />}
+                            <Typography marginTop={1}>Half Day</Typography>
+                        </Box>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        variant="outlined"
+                        autoFocus
+                        onClick={handleClose}
+                        sx={{
+                            width: '100%',
+                            display: currentStatus === ATTENDANCE_ENUM.NOT_MARKED ? 'none' : 'block'
+                        }}
+                    >
+                        Clear
+                    </Button>
+                    <Button
+                        variant="contained"
+                        autoFocus
+                        onClick={handleSubmit}
+                        disabled={status === ATTENDANCE_ENUM.NOT_MARKED}
+                        sx={{
+                            width: '100%',
+                        }}
+                    >
+                        Save
+                    </Button>
+                </DialogActions>
+            </BootstrapDialog>
+        </React.Fragment>
+    );
+}
 
 export default MarkAttendance;
