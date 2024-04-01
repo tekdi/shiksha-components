e11c7104b5617ef3cb585c9ebd67e8109b59427b
rahul_g <rahul_g@tekditechnologies.com>
2024-04-01 10:09:11 +0530

    added attendance report service

--- src/services/AttendanceService.ts
+++ src/services/AttendanceService.ts
@@ -3,10 +3,7 @@ import {
   AttendanceParams,
   BulkAttendanceParams,
   AttendanceByDateParams,
-  TeacherAttendanceByDateParams,
-  AttendanceReports,
-  
-  
+  TeacherAttendanceByDateParams
 } from '../utils/Interfaces';
 
 export const markAttendance = async ({
